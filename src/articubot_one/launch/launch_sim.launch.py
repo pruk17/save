ed2b68e443be5c1259fe4c512e53c4879ea37ae2
Pruk <chaiyapruk6494@gmail.com>
2025-03-01 01:51:19 +0700

    add file

---
+++ src/articubot_one/launch/launch_sim.launch.py
@@ -0,0 +1,85 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='articubot_one' #<--- เปลี่ยนPackageตรงนี้
+
+    # Path to the parameter file for SLAM
+    slam_params = os.path.join(
+        get_package_share_directory(package_name), 
+        'config', 
+        'mapper_params_online_async.yaml'  # Ensure this file exists in the specified path
+    )
+
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+             PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             launch_arguments={
+                 'world': os.path.join(get_package_share_directory(package_name), 'worlds', 'MNP_mapNoBot.world'),
+                 'use_sim_time': 'true' 
+             }.items(),
+    )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'MiniP_bot',#<--- เปลี่ยนชื่อหุ่นยนต์ตรงนี้
+                                   '-x', '0', '-y', '0', '-z', '.4',# Set initial point
+                                   '-R', '0', '-P', '0', '-Y', '1.57'# Set Rotage
+                                ],
+                        output='screen')
+    
+     # Launch SLAM tool with parameters from the provided YAML file
+    slam = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('slam_toolbox'),'launch','online_async_launch.py'
+                )]), 
+                launch_arguments={'use_sim_time': 'true', 'params_file': slam_params}.items()
+    )
+
+    # Node for twist_mux
+    twist_mux = Node(
+        package='twist_mux',
+        executable='twist_mux',
+        output='screen',
+        parameters=[
+            os.path.join(
+                get_package_share_directory(package_name), 
+                'config', 
+                'twist_mux.yaml'  # Ensure this file exists in the specified path
+            )
+        ],
+        remappings=[
+            ('cmd_vel_out', 'diff_cont/cmd_vel_unstamped')  # Remap output topic
+        ]
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+        twist_mux,
+        slam
+    ])
